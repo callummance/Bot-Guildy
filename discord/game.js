a32e287cbbce0a082c829264f4866fed36202ad1
Callum Mance <callummance@gmail.com>
2016-11-22 07:34:31 +0000

    Add game title update on initial connection

--- discord/game.js
+++ discord/game.js
@@ -4,6 +4,10 @@ const conf = require("../config/conf");
 
 
 module.exports.startGameRotate = (client) => {
+    var gamesList = conf().GamesList;
+    var newGameTitle = gamesList[Math.floor(Math.random()*gamesList.length)];
+    client.user.setGame(newGameTitle);
+
     var rule = new schedule.RecurrenceRule();
     rule.hour = 23;
     schedule.scheduleJob(rule, () => {
