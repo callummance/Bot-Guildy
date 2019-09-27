c790c6b8cb1fc48426291137d7c654e0a63c3093
Dennis Tsiang <dennis.tsiang15@imperial.ac.uk>
2019-09-27 18:49:20 +0100

    Add Gfycat support for unknown Hakase requests
    If the bot receives a Hakase request that is not defined, it will grab
    the first sfw gif it can find from searching Gfycat.

--- discord/hakase_responses.js
+++ discord/hakase_responses.js
@@ -1,19 +1,34 @@
-const Promise = require("promise");
 const Logger = require("../logger/logger");
+const request = require('request');
 
-const conf = require("../config/conf");
-const auth = require("../user/auth");
-const userfunction = require("../user/user");
-const Discord = require("discord.js");
 
 module.exports.interpretHakaseQuery = (client, message) => {
     if (message.cleanContent == "meow") {
+        Logger.log("Received meow request");
         message.channel.sendMessage("https://tinyurl.com/y2mlo33q");
     } else if (message.cleanContent == "dance") {
+        Logger.log("Received dance request");
         message.channel.sendMessage("https://tinyurl.com/yyulqpeg");
     } else if (message.cleanContent == "shark") {
+        Logger.log("Received shark request");
         message.channel.sendMessage("https://tinyurl.com/yybbw6au");
     } else {
-        message.channel.sendMessage("Sorry Hakase is busy right now. Go ask Nano instead.");
+        Logger.log("Received unknown request. Searching Gfycat...");
+        let urlQuery = encodeURI(message);
+        request('https://api.gfycat.com/v1/gfycats/search?search_text=hakase%20'+urlQuery, { json: true }, (err, res, body) => {
+            if (err) {
+                Logger.warn(err);
+                message.channel.sendMessage("Sorry Hakase is busy right now. Go ask Nano instead.");
+                return;
+            }
+            let gifs = body["gfycats"];
+            for (let gif of gifs) {
+                if (gif["nsfw"] == "0") {
+                    message.channel.sendMessage(gif["max5mbGif"]);
+                    return;
+                }
+            }
+        });
+        
     }
 }
